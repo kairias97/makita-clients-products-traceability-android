8b9f007b888e13709bc5ad422d53042330c7c92d
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-07-06 13:44:56 -0600

    Se agrega corrección para bug de restaurar contraseña

--- app/src/main/java/ni/com/fetesa/makitamovil/model/PasswordResetResponse.kt
+++ app/src/main/java/ni/com/fetesa/makitamovil/model/PasswordResetResponse.kt
@@ -5,4 +5,4 @@ import com.google.gson.annotations.SerializedName
 /**
  * Created by dusti on 01/07/2018.
  */
-data class PasswordResetResponse(@SerializedName("IsSuccessful") val isSuccesful: Boolean, @SerializedName("Message") val message: String)
+data class PasswordResetResponse(@SerializedName("isSuccessful") val isSuccesful: Boolean, @SerializedName("message") val message: String)
--- app/src/main/java/ni/com/fetesa/makitamovil/model/PasswordResetVerificationResponse.kt
+++ app/src/main/java/ni/com/fetesa/makitamovil/model/PasswordResetVerificationResponse.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 /**
  * Created by dusti on 01/07/2018.
  */
-data class PasswordResetVerificationResponse(@SerializedName("IsValid") val isValid: Boolean, @SerializedName("Message") val message: String,
-                                             @SerializedName("RequestID") val requestID: String)
+data class PasswordResetVerificationResponse(@SerializedName("isValid") val isValid: Boolean, @SerializedName("message") val message: String,
+                                             @SerializedName("requestID") val requestID: String)
